6e1ee223a168a3bd8df02ab21b632c61f2b298b8
orianbi <isaasrori32@gmail.com>
2023-02-16 14:40:27 +0800

    memperbaiki file gambar yg blm berubah

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -6,7 +6,14 @@
     <div class="col-md-8">
         <h1 class="mb-3">{{ $post->title }}</h1>
         <p>By. <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-        <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+        @if ($post->image)
+                            
+                <img src="{{ asset('storage/'. $post->image) }}" class="card-img-top" alt="{{ $post->category->name }}">
+        @else
+            
+                <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+        @endif
+        
         <article class="my-3 fs-5">
             <p>{!! $post->body !!}</p>
 
--- resources/views/post/posts.blade.php
+++ resources/views/post/posts.blade.php
@@ -7,7 +7,16 @@
 @if ($posts->count())
     
       <div class="card mb-3">
-        <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+        @if ($posts[0]->image)
+        <div style="max-height: 450px; overflow: hidden;">
+            <img src="{{ asset('storage/'. $posts[0]->image) }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+        </div>
+        @else
+            
+            <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+        
+        @endif
+
         <div class="card-body text-center">
           <h3 class="card-title"><a href="/post/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
             <small class="text-muted"> <p>By. <a href="/authors/{{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}">{{ $posts[0]->category->name }}</a>  </p></small>
@@ -25,7 +34,14 @@
       <div class="col-md-4 mb-3">
         <div class="card">
           <div class="position-absolute px-3 py-2" style="background-color:rgba(0,0,0,0.7) "><a href="/categories/{{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
-          <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+          @if ($post->image)      
+              
+                <img src="{{ asset('storage/'. $post->image) }}" class="card-img-top" alt="{{ $post->category->name }}">
+          @else
+              
+                <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+          @endif
+          
           <div class="card-body">
             <h5 class="card-title">{{ $post->title }}</h5>
             <small class="text-muted"> <p>By. <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }} </p></small>
